de00411b7c9a6416feca9f0c8a132608e48d7154
daniel <vargass.daniel@hotmail.com>
2020-07-01 11:41:06 -0300

    Fluxo principal definido coom funções a serem chamadas

--- main.c
+++ main.c
@@ -4,9 +4,11 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
+#include <time.h>
+#include <stdlib.h>
 
 #define TAM_BUFFER_READ_FILE 10
+#define TAM_DICIONARIO 26
 
 FILE *cipher;
 FILE *key;
@@ -16,6 +18,14 @@ FILE *open_plaintext(char nome_arq[]);
 void setup_work_directory();
 
 void close_directory();
+
+int encript(char *c, int *key);
+
+char *get_key();
+
+void write_key(int *key);
+
+void write_cipher(char *cifra);
 
 
 int main() {
@@ -28,6 +38,8 @@ int main() {
 
     if (plaintext != NULL) {
         char buffer[TAM_BUFFER_READ_FILE];
+        int cifra;
+        int key;
 
         setup_work_directory();
 
@@ -35,16 +47,40 @@ int main() {
         while (!feof(plaintext)) {
             fgets(buffer, TAM_BUFFER_READ_FILE, plaintext);
 
-            //TODO: para cada caractere transformar em binario
-            //TODO: criar um randon binario para codificar o caractere
-            //TODO: Adicionar a key no arquivo (Append)
-            //TODO: Adicionar a cifra no arquivo (Append)
-            //read_length = fread(plaintext, TAM_BUFFER_READ_FILE, buffer);
+            for (int i = 0; i < TAM_BUFFER_READ_FILE; ++i) {
+                if (buffer[i] != EOF) {
+                    if (buffer[i] != ' ') { //para cada caracter lido, diferente de espaço
+                        key = get_key(); //gerar uma nova chave
+                        cifra = encript(&buffer[i], &key); //encritar o caractere com a chave gerada e salvar a cifra
+                        write_key(&key); //salvar a chave usada
+                        write_cipher(&cifra); //salvar a cifra
+                    }
+                }
+            }
         }
 
     }
     fclose(plaintext);
     close_directory();
+}
+
+/**
+ * Escreve a chave usada no arquivo que contém a chave
+ * @param key chave que foi usada na encriptação
+ */
+void write_key(int *key) {
+    //fazer um append
+    //TODO: implementar a escrita da chave no arquivo key
+}
+
+/**
+ * Faz a cifra de um caractere usando uma chave
+ * @param c caractere a ser encritado
+ * @param key chave usada para encriptar
+ * @return a cifra
+ */
+int encript(char *c, int *key) {
+//    printf("%d", *key);
 }
 
 void close_directory() {
@@ -69,4 +105,26 @@ FILE *open_plaintext(char nome_arq[]) {
     } else {
         printf("nomeArquivo nulo");
     }
+}
+
+/**
+ * Gera uma chave randomica
+ * @return a chave gerada a ser salva no arquivo
+ */
+char *get_key() {
+    srand(time(NULL));
+
+    char key;
+
+    key = (char) rand() % TAM_DICIONARIO;
+
+    return &key;
+}
+
+/**
+ * Escreve no arquivo de cifra a cifra gerada com a chave
+ * @param cifra gerada a ser salva no arquivo
+ */
+void write_cipher(char *cifra) {
+
 }
